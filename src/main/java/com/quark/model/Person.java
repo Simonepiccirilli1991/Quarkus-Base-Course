3b001dcf2a854688e564a9e19de47a832ed78fa3
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-10-09 23:11:39 +0200

    chapter 4 db persistence

--- src/main/java/com/quark/model/Person.java
+++ src/main/java/com/quark/model/Person.java
@@ -1,5 +1,6 @@
 package com.quark.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,6 +9,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
+@Entity(name = "person")
 public class Person {
 
     @Id
